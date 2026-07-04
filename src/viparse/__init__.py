651081384c8aefdc8a8023b975d8282786b988c5
minhtridinh-kayden <minhtridinh.kayden@gmail.com>
2026-07-04 16:53:47 +0700

    VIP-9 Define pipeline layer interfaces (#17)

--- src/viparse/__init__.py
+++ src/viparse/__init__.py
@@ -9,14 +9,28 @@ from viparse.model import (
     NormalizedDoc,
     RawExtraction,
 )
+from viparse.protocols import (
+    DEFAULT_PRIORITY,
+    Engine,
+    Normalizer,
+    OutputFormat,
+    Renderer,
+    Source,
+)
 
 __version__ = "0.0.0"
 
 __all__ = [
+    "DEFAULT_PRIORITY",
     "Chunk",
     "Document",
     "DocumentMetadata",
+    "Engine",
     "NormalizedDoc",
+    "Normalizer",
+    "OutputFormat",
     "RawExtraction",
+    "Renderer",
+    "Source",
     "__version__",
 ]
